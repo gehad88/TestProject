711405e959b1316147c5ad649fc168193fae6c5d
gehad88 <gehadomar007@gmail.com>
2023-04-03 15:01:40 +0200

    UserInterfcae

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace TestProj.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult UserInterface()
+        {
+            return View();
+        }
     }
 }
